46081bb2cdab1971c0c8720cacff442041a41094
Yuki Sato <yuki.sato@lightcurve.io>
2020-06-26 16:11:58 +0200

    Add task input form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,42 @@
 import { bind } from "nd"
 import { map } from "ramda"
 import style from "./style";
+import { useState } from "react"
+
+const isBlank = val => /^\s*$/.test(val);
+
+const NewTask = ({ init }) => {
+  const [newTask, setNewTask] = useState("");
+  return (
+    <div sytle={style.newTask}>
+      <input
+        style={style.input}
+        value={newTask}
+        onChange={e => {
+          setNewTask(e.target.value)
+        }}
+      />
+      <div
+        style={style.add}
+        onClick={() => {
+          if (!isBlank(newTask)) {
+            setNewTask("")
+          }
+        }}
+      >
+        Add
+      </div>
+    </div>
+  )
+}
+
 
 export default bind(
   ({ todos }) => (
     <div style={style.container}>
       <div style={style.todos}>
-        <div>{todos.map(todo => <div>{todo.task}</div>)}</div>
+        <NewTask />
+        {todos.map(todo => <div>{todo.task}</div>)}
       </div>
     </div>
   ),
